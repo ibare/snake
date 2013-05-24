4359d5f93a9f53dbfb82ea690971773c58da8758
ibare <ibare77@gmail.com>
2013-05-24 14:06:30 +0900

    express 극초기 앱 구조 잡기

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function () {
+  app.set('port', process.env.PORT || 4000);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Snake 서버가 ' + app.get('port') + '포트에서 동작합니다.');
+});
